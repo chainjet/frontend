5c6259389af2893a192ec2d4fa217e4e20851529
marian2js <marianomdq1@gmail.com>
2022-08-11 19:12:36 +0200

    feat: add google analytics 4 tracker

--- src/services/GoogleAnalyticsService.ts
+++ src/services/GoogleAnalyticsService.ts
@@ -1,11 +1,20 @@
-import ReactGA, { EventArgs } from 'react-ga'
+import ReactGA from 'react-ga4'
+
+type UaEventOptions = {
+  action: string
+  category: string
+  label?: string
+  value?: number
+  nonInteraction?: boolean
+  transport?: 'beacon' | 'xhr' | 'image'
+}
 
 export const GoogleAnalyticsService = {
   _initialized: false,
 
   _initialize() {
-    ReactGA.initialize(process.env.GA_TRACKING_CODE || 'UA-000000-01', {
-      debug: false,
+    ReactGA.initialize([{ trackingId: process.env.NEXT_PUBLIC_GA_TRACKING_CODE || 'G-0000000000' }], {
+      testMode: !process.env.NEXT_PUBLIC_GA_TRACKING_CODE,
     })
     this._initialized = true
   },
@@ -18,11 +27,11 @@ export const GoogleAnalyticsService = {
     // set timeout needed to give time to refresh the page title
     setTimeout(() => {
       ReactGA.set({ page })
-      ReactGA.pageview(page)
+      ReactGA.send({ hitType: 'pageview', page })
     }, 0)
   },
 
-  sendEvent(event: EventArgs) {
+  sendEvent(event: UaEventOptions) {
     if (!this._initialized) {
       this._initialize()
     }
