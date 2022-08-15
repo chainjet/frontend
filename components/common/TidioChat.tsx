01056b623534d1baf7b7b967736e6892b217649f
marian2js <marianomdq1@gmail.com>
2022-08-15 14:40:16 +0200

    feat: only show tidio chat on production

--- components/common/TidioChat.tsx
+++ components/common/TidioChat.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export class TidioChat extends React.PureComponent<{}> {
   componentDidMount() {
-    ;(function () {
-      const s = document.createElement('script')
-      s.src = 'https://code.tidio.co/any4lrvf0c7wgqshf5wbc8kcqyllvvow.js'
-      s.async = true
-      document.getElementsByTagName('head')[0].appendChild(s)
-    })()
+    if (process.env.NEXT_PUBLIC_ENV === 'production') {
+      ;(function () {
+        const s = document.createElement('script')
+        s.src = 'https://code.tidio.co/any4lrvf0c7wgqshf5wbc8kcqyllvvow.js'
+        s.async = true
+        document.getElementsByTagName('head')[0].appendChild(s)
+      })()
+    }
   }
 
   render() {
