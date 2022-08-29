79698063777732b24ef042ca3c30c81cd40486cb
marian2js <marianomdq1@gmail.com>
2022-08-29 19:51:37 +0200

    feat: add landing

--- components/landing/LandingFooter.tsx
+++ components/landing/LandingFooter.tsx
@@ -1,9 +1,5 @@
-import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint'
-import React from 'react'
-import { Footer10DataSource } from './generated/data.source'
-import Footer from './generated/Footer1'
+const footerHtml: string = require('../../public/dist/footer.html')
 
 export function LandingFooter() {
-  const breakpoint = useBreakpoint()
-  return <Footer id="Footer1_0" key="Footer1_0" dataSource={Footer10DataSource} isMobile={breakpoint.xs} />
+  return <div dangerouslySetInnerHTML={{ __html: footerHtml }} />
 }
--- next.config.js
+++ next.config.js
@@ -1,24 +1,42 @@
 /* eslint-disable */
 const withLess = require('next-plugin-antd-less')
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path')
+// const lessToJS = require('less-vars-to-js')
+// const fs = require('fs')
+// const path = require('path')
+const WithHtmlModule = require('@blunck/next-html')
 require('dotenv').config()
 
 // Where your antd-custom.less file lives
-const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8'))
+// const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8'))
 
-module.exports = withLess({
-  env: {
-    GA_TRACKING_CODE: process.env.GA_TRACKING_CODE,
-    SLACK_APP_ID: process.env.SLACK_APP_ID,
-    CRISP_WEBSITE_ID: process.env.CRISP_WEBSITE_ID,
-  },
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['flowoid.s3.amazonaws.com'],
-    path: '/_next/image',
-    loader: 'default',
-  },
-})
+const withHtml = WithHtmlModule({})
+
+module.exports = withHtml(
+  withLess({
+    images: {
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      domains: ['flowoid.s3.amazonaws.com'],
+      path: '/_next/image',
+      loader: 'default',
+    },
+    rewrites: async () =>
+      process.env.NEXT_PUBLIC_IS_CLOUD
+        ? [
+            {
+              source: '/',
+              destination: '/dist/index.html',
+            },
+            {
+              source: '/pricing',
+              destination: '/dist/pricing.html',
+            },
+          ]
+        : [
+            {
+              source: '/',
+              destination: '/go-register.html',
+            },
+          ],
+  }),
+)
