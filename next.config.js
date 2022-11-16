623fd5c7a74987dcf9658b28245fdbc2d7756e37
marian2js <marianomdq1@gmail.com>
2022-11-15 21:56:48 -0300

    chore: upgrade wagmi

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const withHtml = WithHtmlModule({})
 
 module.exports = withHtml(
   withLess({
+    experimental: { esmExternals: true },
     modifyVars: themeVariables,
     images: {
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
