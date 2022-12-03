70ecc06fd4642f1e9d417d16b222087b95f52f80
marian2js <marianomdq1@gmail.com>
2022-12-03 15:16:36 -0300

    feat: update landing header

--- components/landing/LandingHeader.tsx
+++ components/landing/LandingHeader.tsx
@@ -1,5 +1,36 @@
-const headerHtml = require('../../public/dist/navbar.html')
+import { Button } from 'antd'
+import Link from 'next/link'
+import { useSigner } from '../../src/services/UserHooks'
 
 export function LandingHeader() {
-  return <div dangerouslySetInnerHTML={{ __html: headerHtml }} />
+  const { signer } = useSigner()
+
+  return (
+    <header className="bg-secondary">
+      <div className="container flex items-center w-full px-4 mx-auto max-w-7xl" style={{ height: 113 }}>
+        <div>
+          <Link href="/">
+            <a className="cursor-pointer">
+              <img src="/images/Logo_horizontal_background-less.png" alt="ChainJet Logo" className="w-40 md:w-72" />
+            </a>
+          </Link>
+        </div>
+        <div className="justify-end w-full text-right">
+          {signer ? (
+            <Link href="/dashboard">
+              <Button type="primary" style={{ width: 140, height: 40 }}>
+                Dashboard
+              </Button>
+            </Link>
+          ) : (
+            <Link href="/login">
+              <Button type="primary" style={{ width: 140, height: 40 }}>
+                Connect Wallet
+              </Button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  )
 }
